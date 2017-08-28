99c074a8d0dae7ab7b75b5ccaaa4f540ce7eadac
Daniel Kim <skaghzz@naver.com>
2017-08-28 11:32:28 +0900

    입력 문자열에서 명사 추출

--- app/controllers/chatBot.js
+++ app/controllers/chatBot.js
@@ -2,7 +2,7 @@ var dbconfig = require('../../config/database.js');
 var mysql = require('mysql');
 var connection = mysql.createConnection(dbconfig);
 
-const OpenKoreanText = require('open-korean-text-node').default;
+const OpenKoreanTextProcessor = require('open-korean-text-node').default;
 var stringSimilarity = require('string-similarity');
 
 exports.welcomeChatbot = function(req, res) {
@@ -11,12 +11,27 @@ exports.welcomeChatbot = function(req, res) {
 
 exports.chatbot = function(req, res) {
     //var inputMessage = res.body.inputMessage;
-    var inputMessage = '오늘 부산 날씨 알려줘';
-    var a = OpenKoreanText.tokenizeSync('착한강아지상을 받은 루루')
-    var b = a.toJSON();
-    console.log(b);
+    var inputMessage = '오늘의 부산시 금정구의 날씨를 알려줘';
+    OpenKoreanTextProcessor.normalize(inputMessage).then((normalizedMessage) => {
+        OpenKoreanTextProcessor.tokenize(inputMessage).then((messageToken) => {
+            OpenKoreanTextProcessor.tokensToJsonArray(messageToken).then((JSONmessageTokenArray) => {
+                var nounArray = getNounText(JSONmessageTokenArray);
 
+                res.send(nounArray);
+            })
 
-    var outputMessage = inputMessage;
-    res.send(outputMessage);
+        })
+    })
+
+}
+
+function getNounText(JSONmessageTokenArray) {
+    var nounSpliter = [];
+    for (var idx = 0; idx < JSONmessageTokenArray.length; idx++) {
+        if (JSONmessageTokenArray[idx].pos == 'Noun') {
+            nounSpliter.push(JSONmessageTokenArray[idx].text);
+        }
+    }
+    console.log(nounSpliter);
+    return nounSpliter;
 }
