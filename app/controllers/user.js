8f29facf28b96579749f5a05972ec087f1404242
kdino <kdlee93@gmail.com>
2017-09-02 06:38:46 +0900

    현재위치 저장

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -13,7 +13,25 @@ exports.locations = function(req, res) {
   connection.query('UPDATE USERS SET address_1 = (SELECT num FROM frequent_flooding WHERE si = ? && gu = ? && dong = ?) WHERE device_token = ?', [si, gu, dong, device_token], function(err, rows) {
     if (err) throw err;
 
-    console.log('The solution is: ', rows[0].num);
+    console.log('The solution is: ', rows[0]);
+
+    console.log('The solution is: ', rows);
+    res.send(rows);
+  });
+
+}
+
+exports.curr_location = function(req, res) {
+
+  var device_token = req.query.device_token;
+  var si = req.query.si;
+  var gu = req.query.gu;
+  var dong = req.query.dong;
+
+  connection.query('UPDATE USERS SET address_2 = (SELECT num FROM frequent_flooding WHERE si = ? && gu = ? && dong = ?) WHERE device_token = ?', [si, gu, dong, device_token], function(err, rows) {
+    if (err) throw err;
+
+    console.log('The solution is: ', rows[0]);
 
     console.log('The solution is: ', rows);
     res.send(rows);
@@ -23,7 +41,9 @@ exports.locations = function(req, res) {
 
 exports.saveToken = function(req, res) {
   var userToken = req.query.device_token;
-  connection.query('INSERT INTO USER (device_token) VALUES ('+userToken+');', function(err, rows) {
+  console.log('start');
+
+  connection.query('INSERT INTO USERS (device_token) VALUES ("'+userToken+'");', function(err, rows) {
     if (err) throw err;
 
     console.log('The solution is: ', rows);
