c848632e8077afed0451e8a22902b5d1403d8653
kdino <kdlee93@gmail.com>
2017-09-02 05:10:04 +0900

    토큰 저장, 유저 설정 지역 저장 API 추가

---
+++ app/controllers/user.js
@@ -0,0 +1,33 @@
+var dbconfig = require('../../config/database.js');
+var mysql = require('mysql');
+var connection = mysql.createConnection(dbconfig);
+
+
+exports.locations = function(req, res) {
+
+  var device_token = req.query.device_token;
+  var si = req.query.si;
+  var gu = req.query.gu;
+  var dong = req.query.dong;
+
+  connection.query('UPDATE USERS SET address_1 = (SELECT num FROM frequent_flooding WHERE si = ? && gu = ? && dong = ?) WHERE device_token = ?', [si, gu, dong, device_token], function(err, rows) {
+    if (err) throw err;
+
+    console.log('The solution is: ', rows[0].num);
+
+    console.log('The solution is: ', rows);
+    res.send(rows);
+  });
+
+}
+
+exports.saveToken = function(req, res) {
+  var userToken = req.query.device_token;
+  connection.query('INSERT INTO USER (device_token) VALUES ('+userToken+');', function(err, rows) {
+    if (err) throw err;
+
+    console.log('The solution is: ', rows);
+    res.send(rows);
+  });
+
+}
